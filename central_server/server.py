2ae285a44f6525e72498296bb4672e73c98ba724
Rafael Gallardo <rgallardo.cs@outlook.com>
2024-05-08 22:13:45 -0600

    refactor!: rename core to central_server

---
+++ central_server/server.py
@@ -0,0 +1,23 @@
+import json
+from typing import Annotated
+
+import cv2
+import numpy as np
+from fastapi import Body, FastAPI, File, Form, UploadFile
+
+app = FastAPI()
+
+
+@app.post("/sentinel/api/register_object")
+async def register_object(file: Annotated[UploadFile, File()]):
+    # Read the image file
+    image_bytes = await file.read()
+
+    # Convert the bytes to a numpy array
+    nparr = np.frombuffer(image_bytes, np.uint8)
+
+    # Decode the numpy array to an OpenCV image
+    cv_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    # Return a response
+    return {"message": f"Object registered successfully."}
--- central_server/test_client.py
+++ central_server/test_client.py
@@ -20,7 +20,6 @@ def send_image_and_data(url, image_path):
     # Prepare the files
     files = {
         "file": ("image.jpg", image_file, "image/jpeg"),
-        # 'data': ('data.json', json.dumps(metadata), 'application/json')
     }
 
     # Send the POST request
@@ -31,7 +30,7 @@ def send_image_and_data(url, image_path):
 
 
 if __name__ == "__main__":
-    url = "http://localhost:8001/register_object/"
+    url = "http://localhost:8001/sentinel/api/register_object"
     image_path = "/home/yibbtstll/Pictures/Rafael/test_Face.jpg"
     response = send_image_and_data(url, image_path)
     print(response.text)
